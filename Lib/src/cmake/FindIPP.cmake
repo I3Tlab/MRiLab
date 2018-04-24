eaab7750ba02fe07f8e9926001e3810180b25df6
arwillis@uncc.edu <arwillis@uncc.edu>
2018-04-24 09:23:13 -0400

    Updated FindMatlab.cmake for Mac OS X. Updated FindIPP.cmake reverting WIN32 part to the code in origin.

--- Lib/src/cmake/FindIPP.cmake
+++ Lib/src/cmake/FindIPP.cmake
@@ -20,6 +20,7 @@ ELSE( "$ENV{IPP_ROOT}" STREQUAL "" )
                 PATH_SUFFIXES ipp ipp/include)
             INCLUDE_DIRECTORIES(${IPP_INCLUDE_DIR})
 
+
             SET(IPP_LIB_PATH $ENV{IPP_ROOT}/lib/intel64/)
             FIND_LIBRARY( IPP_S_LIBRARY
                           NAMES "ipps_l"
@@ -49,56 +50,39 @@ ELSE( "$ENV{IPP_ROOT}" STREQUAL "" )
             INCLUDE_DIRECTORIES(${IPP_INCLUDE_DIR})
 
             SET(IPP_LIB_PATH $ENV{IPP_ROOT}/lib)
+    
+            FIND_LIBRARY( IPP_SE_LIBRARY
+                          NAMES ippsemergedem64t
+                          PATHS ${IPP_LIB_PATH})
 
             FIND_LIBRARY( IPP_S_LIBRARY
-                          NAMES "ipps"
+                          NAMES ippsmergedem64t_t
+                          PATHS ${IPP_LIB_PATH})
+
+            FIND_LIBRARY( IPP_VME_LIBRARY
+                          NAMES ippvmemergedem64t
                           PATHS ${IPP_LIB_PATH})
 
             FIND_LIBRARY( IPP_VM_LIBRARY
-                          NAMES "ippvm"
+                          NAMES ippvmmergedem64t_t
+                          PATHS ${IPP_LIB_PATH})
+
+            FIND_LIBRARY( IPP_IOMP5_LIBRARY
+                          NAMES "libiomp5mt"
                           PATHS ${IPP_LIB_PATH})
 
             FIND_LIBRARY( IPP_CORE_LIBRARY
-                          NAMES "ippcore"
+                          NAMES ippcoreem64t_t
                           PATHS ${IPP_LIB_PATH})
 
             SET(IPP_LIBRARIES
+              ${IPP_SE_LIBRARY}
               ${IPP_S_LIBRARY}
+              ${IPP_VME_LIBRARY}
               ${IPP_VM_LIBRARY}
+              ${IPP_IOMP5_LIBRARY}
               ${IPP_CORE_LIBRARY}
             )
-#            FIND_LIBRARY( IPP_SE_LIBRARY
-#                          NAMES ippsemergedem64t
-#                          PATHS ${IPP_LIB_PATH})
-
-#            FIND_LIBRARY( IPP_S_LIBRARY
-#                          NAMES ippsmergedem64t_t
-#                          PATHS ${IPP_LIB_PATH})
-
-#            FIND_LIBRARY( IPP_VME_LIBRARY
-#                          NAMES ippvmemergedem64t
-#                          PATHS ${IPP_LIB_PATH})
-
-#            FIND_LIBRARY( IPP_VM_LIBRARY
-#                          NAMES ippvmmergedem64t_t
-#                          PATHS ${IPP_LIB_PATH})
-
-#            FIND_LIBRARY( IPP_IOMP5_LIBRARY
-#                          NAMES "libiomp5mt"
-#                          PATHS ${IPP_LIB_PATH})
-
-#            FIND_LIBRARY( IPP_CORE_LIBRARY
-#                          NAMES ippcoreem64t_t
-#                          PATHS ${IPP_LIB_PATH})
-
-#            SET(IPP_LIBRARIES
-#              ${IPP_SE_LIBRARY}
-#              ${IPP_S_LIBRARY}
-#              ${IPP_VME_LIBRARY}
-#              ${IPP_VM_LIBRARY}
-#              ${IPP_IOMP5_LIBRARY}
-#              ${IPP_CORE_LIBRARY}
-#            )
 
             MESSAGE (STATUS "IPP_ROOT (IPP 6): $ENV{IPP_ROOT}")
         ENDIF ($ENV{IPP_ROOT} MATCHES .*Composer.*)
@@ -132,7 +116,7 @@ ELSE( "$ENV{IPP_ROOT}" STREQUAL "" )
             )
             MESSAGE (STATUS "IPP_ROOT (IPP 7): $ENV{IPP_ROOT}")
 
-        ELSE ($ENV{IPP_ROOT} MATCHES .*composer.*)    #IPP 6.X
+        ELSE ($ENV{IPP_ROOT} MATCHES .*composer.*)    #IPP 2018 UNIX/Mac OS X
 
             FIND_PATH(IPP_INCLUDE_DIR ipp.h
                     PATHS $ENV{IPP_ROOT}/ipp/include $ENV{IPP_ROOT}/../include /usr/include /usr/local/include
