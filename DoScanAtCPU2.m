2e3b32077f985cc9448afb1ff0d0f2ec810b01f2
arwillis@uncc.edu <arwillis@uncc.edu>
2018-04-25 12:27:50 -0400

    More work on porting the CPU scan code to MATLAB.

--- DoScanAtCPU2.m
+++ DoScanAtCPU2.m
@@ -1,220 +1,3 @@
-
-% /**************************************************************************
-% MEX code for spin discrete evolution using IPP or Framewave
-% and multi-threading (OpenMP) written by Fang Liu (leoliuf@gmail.com).
-% *************************************************************************/
-%
-% /* system header */
-% #include <math.h>
-% #include <stdio.h>
-% #include <stdlib.h>
-% #include <string.h>
-% #include <vector>
-% /* MEX header */
-% #include <mex.h>
-% #include "matrix.h"
-% /* OpenMP header*/
-% #include <omp.h>
-% /* Intel IPP header */
-% #ifdef IPP
-% #include <ipp.h>
-% #endif
-% /* AMD Framewave header */
-% #ifdef FW
-% #include <fwSignal.h>
-% #include <fwBase.h>
-% #define Ipp32f                  Fw32f
-% #define ippAlgHintFast          fwAlgHintFast
-% #define ippsMalloc_32f          fwsMalloc_32f
-% #define ippsFree                fwsFree
-% #define ippsZero_32f            fwsZero_32f
-% #define ippsZero_64f            fwsZero_64f
-% #define ippsSum_32f             fwsSum_32f
-% #define ippsCopy_32f            fwsCopy_32f
-% #define ippsAddC_32f            fwsAddC_32f
-% #define ippsAddC_32f_I          fwsAddC_32f_I
-% #define ippsAdd_32f             fwsAdd_32f
-% #define ippsAdd_32f_I           fwsAdd_32f_I
-% #define ippsMulC_32f            fwsMulC_32f
-% #define ippsMulC_32f_I          fwsMulC_32f_I
-% #define ippsMul_32f             fwsMul_32f
-% #define ippsMul_32f_I           fwsMul_32f_I
-% #define ippsDiv_32f             fwsDiv_32f
-% #define ippsDivC_32f            fwsDivC_32f
-% #define ippsInv_32f_A24         fwsInv_32f_A24
-% #define ippsThreshold_LT_32f_I  fwsThreshold_LT_32f_I
-% #define ippsExp_32f_I           fwsExp_32f_I
-% #define ippsArctan_32f          fwsArctan_32f
-% #define ippsSqr_32f             fwsSqr_32f
-% #define ippsSqr_32f_I           fwsSqr_32f_I
-% #define ippsSqrt_32f_I          fwsSqrt_32f_I
-% #define ippsSin_32f_A24         fwsSin_32f_A24
-% #define ippsCos_32f_A24         fwsCos_32f_A24
-% #define ippsPolarToCart_32f     fwsPolarToCart_32f
-% #define ippsCartToPolar_32f     fwsCartToPolar_32f
-% #endif
-%
-% #if defined(_WIN32) || defined(_WIN64)
-% #include <windows.h>
-% #define fmin min
-% #endif
-%
-% #define PI      3.14159265359 /* pi constant */
-%
-% /* includes CPU kernel */
-% #include "BlochKernel.h"
-% //extern "C" bool mxUnshareArray(mxArray *array_ptr, bool noDeepCopy);
-% extern "C" int mxUnshareArray(mxArray *array_ptr, int noDeepCopy);
-%
-% /* MEX entry function */
-% void mexFunction(int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[])
-%
-% {
-%
-% /* pointers for VObj */
-%     double *Gyro;
-%     mwSize SpinMxNum, SpinMxSliceNum, SpinMxDimNum, ThreadNum;
-%     const mwSize *SpinMxDims;
-% 	float *MzBase, *MyBase, *MxBase, *Mz, *My, *Mx, *RhoBase, *T1Base, *T2Base, *Rho, *T1, *T2;
-%
-% /* pointers for VMag */
-%     float *dB0Base, *dWRndBase, *GzgridBase, *GygridBase, *GxgridBase;
-%     float *dB0, *dWRnd, *Gzgrid, *Gygrid, *Gxgrid;
-%
-% /* pointers for VCoi */
-%     float *RxCoilx, *RxCoily;
-%     float *TxCoilmg, *TxCoilpe, *TxCoilmgBase, *TxCoilpeBase;
-% 	double *RxCoilDefault, *TxCoilDefault;
-%
-% /* pointers for VCtl */
-%     double *CS;
-%     int *TRNum, *RunMode, *MaxThreadNum;
-%
-% /* pointers for VSeq */
-%     double *utsLine, *tsLine, *rfAmpLine, *rfPhaseLine, *rfFreqLine, *rfCoilLine, *GzAmpLine, *GyAmpLine, *GxAmpLine, *ADCLine, *ExtLine, *flagsLine;
-%
-% /* pointers for VVar */
-%     double *t, *dt, *rfAmp, *rfPhase, *rfFreq, *rfCoil, *rfRef, *GzAmp, *GyAmp, *GxAmp, *ADC, *Ext, *KzTmp, *KyTmp, *KxTmp;
-%     int *utsi, *rfi, *Gzi, *Gyi, *Gxi, *ADCi, *Exti, *TRCount;
-%
-% /* pointers for VSig */
-%     double *Sx, *Sy, *Kz, *Ky, *Kx, *Muts;
-% 	float *MxsBase, *MysBase, *MzsBase;
-%
-%
-% /* loop control */
-%     mwIndex i=0, j=0, s=0, Signali=0, Spini, Typei, RxCoili, TxCoili;
-% 	int Slicei;
-%     mwSize MaxStep, MaxutsStep, MaxrfStep, MaxGzStep, MaxGyStep, MaxGxStep, *SpinNum, *TypeNum, *TxCoilNum, *RxCoilNum, *SignalNum;
-%     double flag[6];
-%
-% /* IPP or FW buffer */
-%     Ipp32f *buffer1, *buffer2, *buffer3, *buffer4, *buffer;
-%
-% /* Function status */
-%     int ExtCall;
-%
-% /* force breaking Copy-on-Write */
-%     mxUnshareArray(const_cast<mxArray *>(mexGetVariablePtr("global", "VObj")), true);
-%     mxUnshareArray(const_cast<mxArray *>(mexGetVariablePtr("global", "VMag")), true);
-%     mxUnshareArray(const_cast<mxArray *>(mexGetVariablePtr("global", "VCoi")), true);
-%     mxUnshareArray(const_cast<mxArray *>(mexGetVariablePtr("global", "VCtl")), true);
-%     mxUnshareArray(const_cast<mxArray *>(mexGetVariablePtr("global", "VVar")), true);
-%     mxUnshareArray(const_cast<mxArray *>(mexGetVariablePtr("global", "VSeq")), true);
-%     mxUnshareArray(const_cast<mxArray *>(mexGetVariablePtr("global", "VSig")), true);
-%
-% /* assign pointers */
-%     Gyro            = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "Gyro"));
-%     MzBase          = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "Mz"));
-%     MyBase          = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "My"));
-%     MxBase          = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "Mx"));
-%     RhoBase         = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "Rho"));
-%     T1Base          = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "T1"));
-%     T2Base          = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "T2"));
-%     SpinNum         = (mwSize*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "SpinNum"));
-%     TypeNum         = (mwSize*) mxGetData(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "TypeNum"));
-%
-%     dB0Base         = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VMag"), 0, "dB0"));
-%     dWRndBase       = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VMag"), 0, "dWRnd"));
-%     GzgridBase      = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VMag"), 0, "Gzgrid"));
-%     GygridBase      = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VMag"), 0, "Gygrid"));
-%     GxgridBase      = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VMag"), 0, "Gxgrid"));
-%
-%     TxCoilmgBase    = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCoi"), 0, "TxCoilmg"));
-%     TxCoilpeBase    = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCoi"), 0, "TxCoilpe"));
-%     RxCoilx         = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCoi"), 0, "RxCoilx"));
-%     RxCoily         = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCoi"), 0, "RxCoily"));
-%     TxCoilNum       = (mwSize*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCoi"), 0, "TxCoilNum"));
-%     RxCoilNum       = (mwSize*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCoi"), 0, "RxCoilNum"));
-%     TxCoilDefault   = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCoi"), 0, "TxCoilDefault"));
-%     RxCoilDefault   = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCoi"), 0, "RxCoilDefault"));
-%
-%     CS              = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VCtl"), 0, "CS"));
-%     TRNum           = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VCtl"), 0, "TRNum"));
-%     RunMode         = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VCtl"), 0, "RunMode"));
-%     MaxThreadNum    = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VCtl"), 0, "MaxThreadNum"));
-%
-% 	utsLine         = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "utsLine"));
-%     tsLine          = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "tsLine"));
-%     rfAmpLine       = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "rfAmpLine"));
-%     rfPhaseLine     = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "rfPhaseLine"));
-%     rfFreqLine      = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "rfFreqLine"));
-%     rfCoilLine      = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "rfCoilLine"));
-%     GzAmpLine       = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "GzAmpLine"));
-%     GyAmpLine       = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "GyAmpLine"));
-%     GxAmpLine       = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "GxAmpLine"));
-%     ADCLine         = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "ADCLine"));
-%     ExtLine         = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "ExtLine"));
-%     flagsLine       = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "flagsLine"));
-%
-% 	MaxStep         = mxGetNumberOfElements(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "tsLine"));
-%     MaxutsStep      = mxGetNumberOfElements(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "utsLine"));
-%     MaxrfStep       = mxGetNumberOfElements(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "rfAmpLine"));
-%     MaxGzStep       = mxGetNumberOfElements(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "GzAmpLine"));
-%     MaxGyStep       = mxGetNumberOfElements(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "GyAmpLine"));
-%     MaxGxStep       = mxGetNumberOfElements(mxGetField(mexGetVariablePtr("global", "VSeq"), 0, "GxAmpLine"));
-%
-%     t               = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "t"));
-%     dt              = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "dt"));
-%     rfAmp           = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "rfAmp"));
-%     rfPhase         = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "rfPhase"));
-%     rfFreq          = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "rfFreq"));
-%     rfCoil          = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "rfCoil"));
-%     rfRef           = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "rfRef"));
-%     GzAmp           = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "GzAmp"));
-%     GyAmp           = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "GyAmp"));
-%     GxAmp           = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "GxAmp"));
-%     ADC             = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "ADC"));
-%     Ext             = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "Ext"));
-%     KzTmp           = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "Kz"));
-%     KyTmp           = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "Ky"));
-%     KxTmp           = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "Kx"));
-%     utsi            = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "utsi"));
-%     rfi             = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "rfi"));
-%     Gzi             = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "Gzi"));
-%     Gyi             = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "Gyi"));
-%     Gxi             = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "Gxi"));
-%     ADCi            = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "ADCi"));
-%     Exti            = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "Exti"));
-%     TRCount         = (int*)    mxGetData(mxGetField(mexGetVariablePtr("global", "VVar"), 0, "TRCount"));
-%
-%     Sy              = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "Sy"));
-%     Sx              = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "Sx"));
-%     Kz              = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "Kz"));
-%     Ky              = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "Ky"));
-%     Kx              = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "Kx"));
-%     MzsBase         = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "Mz"));
-%     MysBase         = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "My"));
-%     MxsBase         = (float*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "Mx"));
-%     Muts            = (double*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "Muts"));
-%     SignalNum       = (mwSize*) mxGetData(mxGetField(mexGetVariablePtr("global", "VSig"), 0, "SignalNum"));
-%
-%  /* get dimensions of spin matrix */
-%     SpinMxDimNum    = mxGetNumberOfDimensions(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "Mz"));
-%     SpinMxDims      = (mwSize*) mxCalloc(SpinMxDimNum, sizeof(mwSize));
-%     SpinMxDims      = mxGetDimensions(mxGetField(mexGetVariablePtr("global", "VObj"), 0, "Mz"));
-%     SpinMxNum       = SpinMxDims[0] * SpinMxDims[1];
-%
 function A = DoScanAtCPU2()
 global VSeq;
 global VObj;
@@ -223,6 +6,9 @@ global VMag;
 global VCoi;
 global VVar;
 global VSig;
+%    const mwSize *SpinMxDims;
+%	float *MzBase, *MyBase, *MxBase, *Mz, *My, *Mx, *RhoBase, *T1Base, *T2Base, *Rho, *T1, *T2;
+
 %/* loop control */
 i=int32(0); j=int32(0); s=int32(0); Signali=int32(0); Spini=int32(0); Typei=int32(0); RxCoili=int32(0); TxCoili=int32(0);
 Slicei=int32(0);
@@ -346,10 +132,14 @@ end
 %         (since the extra ones do nothing and waste resources) */
 %
 % /* set buffer */
-buffer1 = zeros(SpinMxSliceNum,SpinMxNum,'single');
-buffer2 = zeros(SpinMxSliceNum,SpinMxNum,'single');
-buffer3 = zeros(SpinMxSliceNum,SpinMxNum,'single');
-buffer4 = zeros(SpinMxSliceNum,SpinMxNum,'single');
+%buffer1 = zeros(SpinMxSliceNum,SpinMxNum,'single');
+%buffer2 = zeros(SpinMxSliceNum,SpinMxNum,'single');
+%buffer3 = zeros(SpinMxSliceNum,SpinMxNum,'single');
+%buffer4 = zeros(SpinMxSliceNum,SpinMxNum,'single');
+buffer1 = zeros(1, SpinMxSliceNum*SpinMxNum, 'single');
+buffer2 = zeros(1, SpinMxSliceNum*SpinMxNum, 'single');
+buffer3 = zeros(1, SpinMxSliceNum*SpinMxNum, 'single');
+buffer4 = zeros(1, SpinMxSliceNum*SpinMxNum, 'single');
 buffer = zeros(1,1,'single');
 %
 % /* start simulator execution loop */
@@ -466,6 +256,7 @@ while (i < MaxStep-1)
         if (Ext ~= 0)
             %ExtCall = mexEvalString("DoExtPlugin");
             %ExtCall = DoExtPlugin;
+            disp("DoExtPlugin: ERROR NOT IMPLEMENTED");
             ExtCall = false;
             if (ExtCall)
                 disp("Extended process encounters ERROR!");
@@ -541,7 +332,7 @@ while (i < MaxStep-1)
         
         switch (RunMode)
             case 1 %/* spin rotation simulation & rf simulation */
-                aaa=1;
+                disp("SpinRotation: ERROR NOT IMPLEMENTED");
                 %MzsBase = MzBase;
                 %MysBase = MyBase;
                 %MxsBase = MxBase;
@@ -558,13 +349,18 @@ while (i < MaxStep-1)
         for Spini = 0:(SpinNum-1)
             %/* signal acquisition */
             if (ADC == 1)
-                
+                aaa = 1;
+                % SpinMxNum = num pixels (x,y)
+                % SpinMxSliceNum = num slices (3)
+                % SpinNum = 1?, Typei = 0?
                 %Mx = MxBase + (Typei*(*SpinNum)*SpinMxSliceNum*SpinMxNum + Spini*SpinMxSliceNum*SpinMxNum);
                 %My = MyBase + (Typei*(*SpinNum)*SpinMxSliceNum*SpinMxNum + Spini*SpinMxSliceNum*SpinMxNum);
-                
+                Mx = MxBase((Typei*SpinNum*SpinMxSliceNum*SpinMxNum + Spini*SpinMxSliceNum*SpinMxNum+1):end);
+                My = MyBase((Typei*SpinNum*SpinMxSliceNum*SpinMxNum + Spini*SpinMxSliceNum*SpinMxNum+1):end);
                 for RxCoili = 0:(RxCoilNum-1)%  /* signal acquisition per Rx coil */
                     %/* RxCoil sensitivity */
                     if (RxCoilDefault == 0)
+                        disp("RxCoilDefault==0: ERROR NOT IMPLEMENTED");
                         %ippsMul_32f(Mx, RxCoilx+RxCoili*SpinMxSliceNum*SpinMxNum, buffer1, SpinMxSliceNum*SpinMxNum);
                         %ippsMul_32f(My, RxCoily+RxCoili*SpinMxSliceNum*SpinMxNum, buffer2, SpinMxSliceNum*SpinMxNum);
                         %ippsAdd_32f(buffer1, buffer2, buffer3, SpinMxSliceNum*SpinMxNum);
@@ -577,6 +373,10 @@ while (i < MaxStep-1)
                         %ippsCopy_32f(buffer3, buffer1, SpinMxSliceNum*SpinMxNum);
                         %ippsCopy_32f(buffer4, buffer2, SpinMxSliceNum*SpinMxNum);
                     else
+                        buffer1 = Mx(1:SpinMxSliceNum*SpinMxNum);
+                        buffer2 = My(1:SpinMxSliceNum*SpinMxNum);
+                        buffer3 = Mx(1:SpinMxSliceNum*SpinMxNum);
+                        buffer4 = My(1:SpinMxSliceNum*SpinMxNum);                        
                         %ippsCopy_32f(Mx, buffer1, SpinMxSliceNum*SpinMxNum);
                         %ippsCopy_32f(My, buffer2, SpinMxSliceNum*SpinMxNum);
                         %ippsCopy_32f(Mx, buffer3, SpinMxSliceNum*SpinMxNum);
@@ -585,6 +385,7 @@ while (i < MaxStep-1)
                     
                     %/* rfRef for demodulating rf Phase */
                     if (rfRef ~= 0)
+                        disp("rfRef ~= 0: ERROR NOT IMPLEMENTED");
                         %ippsMulC_32f_I((float)cos(-(*rfRef)), buffer1, SpinMxSliceNum*SpinMxNum);
                         %ippsMulC_32f_I((float)-sin(-(*rfRef)), buffer2, SpinMxSliceNum*SpinMxNum);
                         %ippsAdd_32f_I(buffer2, buffer1, SpinMxSliceNum*SpinMxNum);
@@ -593,10 +394,12 @@ while (i < MaxStep-1)
                         %ippsMulC_32f_I((float)cos(-(*rfRef)), buffer4, SpinMxSliceNum*SpinMxNum);
                         %ippsAdd_32f_I(buffer4, buffer3, SpinMxSliceNum*SpinMxNum);
                     else
+                        disp("rfRef ~= 0 else: ERROR NOT IMPLEMENTED");                        
                         %ippsCopy_32f(buffer4, buffer3, SpinMxSliceNum*SpinMxNum);
                     end
                     
                     %/* signal summation */
+                    disp("signal summation: ERROR NOT IMPLEMENTED");                        
                     %ippsSum_32f(buffer1, SpinMxSliceNum*SpinMxNum, buffer, ippAlgHintFast);
                     %Sx[Typei*(*RxCoilNum)*(*SignalNum)+RxCoili*(*SignalNum)+Signali] += (double)*buffer;
                     
